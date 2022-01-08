ff771996ed8288896db1760b0487b72c5dee01be
dattatreya05 <dattatreyamamidipaka@gmail.com>
2022-01-08 12:53:45 +0530

    movieform

--- src/App.js
+++ src/App.js
@@ -152,43 +152,43 @@ export default function App() {
   //   )
   // }
 
-  function AddColor(){
-    const [color, setColor] = useState('purple');
-    const styles = { background: color };
-    // const [colorList, setColorList] = useState(["yellow", "pink", "teal"]);
+  // function AddColor(){
+  //   const [color, setColor] = useState('purple');
+  //   const styles = { background: color };
+  //   // const [colorList, setColorList] = useState(["yellow", "pink", "teal"]);
 
-    // typing -> onChange -> content (event.target.value) -> setColor -> Inform react color is updated
-    return(
-      <div>
-        <input
-          value={color}
-          style={styles} onChange={(event) => { 
-          // console.log(event.target.value);  // for to get value in the console output we are writing this console.log()
-          setColor(event.target.value);
-        }}
-        placeholder="Enter a color" />
-        {/* <button onClick={() => setColorList([...colorList, color])}>Add Color</button>
-        {colorList.map((clr) =>
-        (<ColorBox color={clr}/> ))} */}
+  //   // typing -> onChange -> content (event.target.value) -> setColor -> Inform react color is updated
+  //   return(
+  //     <div>
+  //       <input
+  //         value={color}
+  //         style={styles} onChange={(event) => { 
+  //         // console.log(event.target.value);  // for to get value in the console output we are writing this console.log()
+  //         setColor(event.target.value);
+  //       }}
+  //       placeholder="Enter a color" />
+  //       {/* <button onClick={() => setColorList([...colorList, color])}>Add Color</button>
+  //       {colorList.map((clr) =>
+  //       (<ColorBox color={clr}/> ))} */}
         
-      </div>
-    )
-  }
+  //     </div>
+  //   )
+  // }
 
 
-  function ColorBox({color}){
-    const styles = {
-      height : "25px",
-      width: "250px",
-      background: color,
-      marginTop:"10px"
-    }
-    return(
-      <div style = {styles}>
+  // function ColorBox({color}){
+  //   const styles = {
+  //     height : "25px",
+  //     width: "250px",
+  //     background: color,
+  //     marginTop:"10px"
+  //   }
+  //   return(
+  //     <div style = {styles}>
 
-      </div>
-    )
-  }
+  //     </div>
+  //   )
+  // }
 
   function Movie({name, poster, rating, summary}){
     // conditional styling
