3087edade11d494c7eaa8894daa0b040c4acf9ad
dattatreya05 <dattatreyamamidipaka@gmail.com>
2022-06-22 16:18:54 +0530

    added global heroku

--- src/global.js
+++ src/global.js
@@ -1,1 +1,3 @@
-export const API = "https://619cfba768ebaa001753ce3a.mockapi.io";
+// export const API = "https://619cfba768ebaa001753ce3a.mockapi.io";
+
+export const API = "https://dattatreya.herokuapp.com";
